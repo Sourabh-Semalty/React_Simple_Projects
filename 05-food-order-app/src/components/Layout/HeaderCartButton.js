b77c4806ac4d48afbc90df3c8a82bcb1f8bf6fe0
Sourabh Semalty <42134981+Sourabh-Semalty@users.noreply.github.com>
2022-04-13 00:03:27 +0530

    Basic food app completion

--- 05-food-order-app/src/components/Layout/HeaderCartButton.js
+++ 05-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,26 @@
-import React,{useContext} from 'react';
+import React,{useContext,useEffect,useState} from 'react';
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../context/cart-context";
 import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighLighted] = useState(false);
   const cartCtx = useContext(CartContext);
-  const numberOfCartItems = cartCtx.items.reduce((currentNumber,item) => (currentNumber+item.amount),0)
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => (currentNumber + item.amount), 0)
+  
+  useEffect(() => {
+    if (cartCtx.items.length === 0) return;
+    setBtnIsHighLighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighLighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    }
+  },[cartCtx.items])
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
   return (
-    <button onClick={props.onClick} className={classes.button}>
+    <button onClick={props.onClick} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
--- 05-food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ 05-food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,23 @@
+import {useRef,useState} from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid,setAmountIsValid] = useState(true)
+  const amountInputRef = useRef();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredAmount = +amountInputRef.current.value;
+    if (enteredAmount < 1 || enteredAmount > 5) {
+      setAmountIsValid(false);
+      return;    
+    }
+    props.onAddToCart(enteredAmount);
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
@@ -16,6 +29,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+Add</button>
+      {!amountIsValid && <p>Please enter a valid Amount {1 - 5}.</p>}
     </form>
   );
 };
