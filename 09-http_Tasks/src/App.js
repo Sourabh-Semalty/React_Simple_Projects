6699248a8619bbd1985d7d532cdf7806060db7ec
Sourabh Semalty <42134981+Sourabh-Semalty@users.noreply.github.com>
2022-04-19 16:41:16 +0530

    Custom Hooks with Project

--- 09-http_Tasks/src/App.js
+++ 09-http_Tasks/src/App.js
@@ -1,42 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks();
+    const transformObj = (data) => {
+      const loadedTasks = [];
+      for (const taskKey in data)
+        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: "https://react-http-76b78-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformObj
+    );
   }, []);
 
   const taskAddHandler = (task) => {
